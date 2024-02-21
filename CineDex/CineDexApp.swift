664c32eb523371f848a8909860713bd0fccaac4d
Andres Zamora <azviquez93@icloud.com>
2024-02-20 18:44:00 -0600

    Initial Commit

---
+++ CineDex/CineDexApp.swift
@@ -0,0 +1,20 @@
+//
+//  CineDexApp.swift
+//  CineDex
+//
+//  Created by Andrés Zamora on 20/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CineDexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
